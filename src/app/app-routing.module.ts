eab73c39c79ba67a4d139cb5df4e402259b7ba05
Kim Tuico <kvtuico@up.edu.ph>
2022-06-27 08:38:28 +0800

    final

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { CharactersComponent } from './characters/characters.component';
 import { CharacterDetailComponent } from './character-detail/character-detail.component';
 import { WeaponsComponent } from './weapons/weapons.component';
@@ -9,6 +10,7 @@ import { ArtifactDetailComponent } from './artifact-detail/artifact-detail.compo
 import { SearchComponent } from './search/search.component';
 
 const routes: Routes = [
+  { path: '', component: DashboardComponent },
   { path: 'characters', component: CharactersComponent },
   { path: 'characters/:id', component: CharacterDetailComponent },
   { path: 'weapons', component: WeaponsComponent },
@@ -16,6 +18,7 @@ const routes: Routes = [
   { path: 'artifacts', component: ArtifactsComponent },
   { path: 'artifacts/:id', component: ArtifactDetailComponent },
   { path: 'search/:searchTerm', component:SearchComponent },
+  { path: '**', redirectTo:''}
 ];
 
 @NgModule({
